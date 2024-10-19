1809855ec2fa3eefad79257b537f55e40cc45446
Fenil-13 <fjmoradiya@gmail.com>
2024-10-20 01:03:49 +0530

    Implement coin list screen UI along with domain layer setup

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/CoinUi.kt
@@ -0,0 +1,47 @@
+package com.plcoding.cryptotracker.crypto.presentation
+
+import android.icu.text.NumberFormat
+import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    @DrawableRes val iconRes: Int,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24hr: DisplayableNumber
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formattedValue: String
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        iconRes = getDrawableIdForCoin(symbol),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        changePercent24hr = changePercent24hr.toDisplayableNumber()
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale("en", "in")).apply {
+        maximumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formattedValue = formatter.format(this)
+    )
+}
---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,11 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Immutable
+import com.plcoding.cryptotracker.crypto.presentation.CoinUi
+
+@Immutable // this will tell the compiler to this class is immutable, if change we replace with new instance the value
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: CoinUi? = null
+)
